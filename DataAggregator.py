a4c6897c3703e77dbf38ad383abb9f5c4d6fc4e7
fchrist <finlay.christie@ukaea.uk>
2021-03-19 11:51:43 +0000

    updated model

--- DataAggregator.py
+++ DataAggregator.py
@@ -7,16 +7,16 @@ Created on Mon Dec 28 16:36:36 2020
 
 import pandas as pd
 
-df =  pd.read_csv (r'C:\Users\Fin Christie\Documents\Uni\4th Year\EM401\Data\Windfarm_Data_Analysis.csv')
-df_list = df["Beatrice"].tolist()
+df =  pd.read_csv (r'C:/SPB_Data/EM401_Code/Green_Hydrogen_Production_Project/ff_Model_v0.6.6/timeseries_data/scotland_ND_hourly.csv')
+df_list = df["region2"].tolist()
 aggregated_data = []
 hourly_data = []
 
 for row, val in enumerate(df_list):
     hourly_data.append(df_list[row])
     
-    if len(hourly_data) == 672:
-        aggregated_data.append((sum(hourly_data)/672))
+    if len(hourly_data) == 24:
+        aggregated_data.append((sum(hourly_data)))
         hourly_data.clear()
         
         
