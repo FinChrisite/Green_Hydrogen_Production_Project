7164d1705239eea1d1d2b9ee097fb333bd4052ae
fchrist <finlay.christie@ukaea.uk>
2021-03-27 10:53:50 +0000

    Added function to save model results in designated file space

--- DataAggregator.py
+++ DataAggregator.py
@@ -7,8 +7,8 @@ Created on Mon Dec 28 16:36:36 2020
 
 import pandas as pd
 
-df =  pd.read_csv (r'C:/SPB_Data/EM401_Code/Green_Hydrogen_Production_Project/ff_Model_v0.6.6/timeseries_data/scotland_ND_hourly.csv')
-df_list = df["region2"].tolist()
+df =  pd.read_csv (r'C:/SPB_Data/EM401_Code/Green_Hydrogen_Production_Project/ff_Model_v0.6.6/timeseries_data/Synth_1kW_osw_hourly.csv')
+df_list = df["region1-1"].tolist()
 aggregated_data = []
 hourly_data = []
 
