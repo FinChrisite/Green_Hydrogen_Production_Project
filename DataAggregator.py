e63a477fea85bd4efe14dd849a13426ac12cfa8a
fchrist <finlay.christie@ukaea.uk>
2021-03-31 21:08:52 +0100

    Final update to model

--- DataAggregator.py
+++ DataAggregator.py
@@ -6,11 +6,17 @@ Created on Mon Dec 28 16:36:36 2020
 """
 
 import pandas as pd
+import numpy as np
 
-df =  pd.read_csv (r'C:/SPB_Data/EM401_Code/Green_Hydrogen_Production_Project/ff_Model_v0.6.6/timeseries_data/Synth_1kW_osw_hourly.csv')
+df =  pd.read_csv (r'C:/SPB_Data/EM401_Code/Green_Hydrogen_Production_Project/ff_Model_v0.6.6/timeseries_data/Beatrice_raw_output_hourly.csv')
 df_list = df["region1-1"].tolist()
 aggregated_data = []
 hourly_data = []
+
+df_arr = np.array(df_list)
+df_arr[np.isnan(df_arr)] = 0
+df_list = df_arr.tolist()
+
 
 for row, val in enumerate(df_list):
     hourly_data.append(df_list[row])
